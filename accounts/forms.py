c7a0a44d437d8708e7d0839c316f4b1e57969334
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-28 08:19:48 +0545

    add frontend templates and forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Post, Comment
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -12,11 +12,19 @@ class RegisterForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.help_text = None
-
+        for f in self.fields.values(): f.help_text = None
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
